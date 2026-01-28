18284d1427fb8c3320c92f39292edf99cc99aa98
Rezwan66 <shaikhrezwan66@gmail.com>
2026-01-28 04:59:36 +0100

    feat(auth): add better-auth to backend project with login and register

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || 'http://localhost:4000',
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.get('/', (req, res) => {
+  res.send('SkillBridge Server is Running 🎓');
+});
+
+export default app;
